5aa8b5ddb10d3de9c3db28fda4a30cbb76fa3a7b
upendrakaka <uppi5747@gmail.com>
2017-06-28 10:31:45 +0530

    create Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,11 @@
+
+public class Lab1{
+   public static void main(String args[]){
+   
+	   System.out.println( "This is first lab");
+	   System.out.println("Save the data")
+   
+   }
+
+}
+
---
+++ Lab2.java
@@ -0,0 +1,8 @@
+public class Lab2{
+    public static void main(String args[]){
+     System.out.println("Checking");
+    
+    
+    }
+
+}
